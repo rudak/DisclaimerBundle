2af66ee83c46ce82d3b3392276a5f5461f322a19
rudak <roiduboul@gmail.com>
2014-08-18 22:51:05 +0200

    fixture always keep the id number 1

--- DataFixtures/ORM/LoadDefaultData.php
+++ DataFixtures/ORM/LoadDefaultData.php
@@ -15,23 +15,32 @@ class LoadDisclaimerData implements FixtureInterface
 
         $disclaimer = new DisclaimerData();
 
+        $disclaimer->setId(1);
         $disclaimer->setLatestUpdate(new \Datetime());
+
         $disclaimer->setCnil(false);
         $disclaimer->setCnilNumber('123456789');
+
         $disclaimer->setEditorEmail('editor@email.com');
         $disclaimer->setEditorManager('editorManager');
+
         $disclaimer->setHoster('hoster');
         $disclaimer->setHosterAddress('hoster address');
+
         $disclaimer->setOwnerName('owner name');
         $disclaimer->setOwnerAddress('owner address');
         $disclaimer->setOwnerCompanyName('owner company name');
         $disclaimer->setOwnerStatus('owner status director');
+
         $disclaimer->setSiteCreatorAgency('site creator agency');
         $disclaimer->setSiteCreatorName('site creator name');
         $disclaimer->setSiteCreatorUrl('www.site-creator-url.com');
         $disclaimer->setSiteUrl('www.site-url.com');
-        $disclaimer->setId(1);
+
         $disclaimer->setWebmasterName('webmaster Name');
+
+        $metadata = $manager->getClassMetaData(get_class($disclaimer));
+        $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
 
         $manager->persist($disclaimer);
         $manager->flush();
