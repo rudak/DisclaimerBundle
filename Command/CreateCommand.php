4675dab680cec09c67776da406c88bea30a3cb4f
rudak <roiduboul@gmail.com>
2014-08-19 01:56:30 +0200

    work in progress

--- Command/CreateCommand.php
+++ Command/CreateCommand.php
@@ -8,7 +8,8 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Rudak\Bundle\DisclaimerBundle\Entity\DisclaimerData;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class CreateCommand extends ContainerAwareCommand
 {
@@ -19,77 +20,156 @@ class CreateCommand extends ContainerAwareCommand
     {
         $this
             ->setName('disclaimer:init')
-            ->setDescription('Disclaimer\'s data initialization')
-            ->setDefinition($this->getDefinitions());
+            ->setDescription('Initialisation du disclaimer');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        foreach ($this->getDisclaimerAttributes() as $attributes => $textAnswer) {
-            $values[$attributes] = $input->getArgument($attributes);
-        }
-        $creator = $this->getContainer()->get('disclaimer.creator');
-        $nb      = $creator->create($values);
-        $output->writeln('The data has been correctly initialized');
-    }
-
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-        foreach ($this->getDisclaimerAttributes() as $attributes => $textAnswer) {
-            if (!$input->getArgument($attributes)) {
-                $entry = $this->getHelper('dialog')->askAndValidate(
-                    $output,
-                    $textAnswer,
-                    function ($entry) {
-                        if (empty($entry)) {
-                            throw new \Exception('This answer can not be empty !');
-                        }
-                        return $entry;
-                    }
-                );
-
-                $input->setArgument($attributes, $entry);
+        $answers = array();
+        foreach ($this->getInformations() as $item) {
+            // Si cet item est soumis a la validation d'une condition
+            if (isset($item['condition'])) {
+                if ($this->checkCondition($answers[$item['condition']['item']], $item['condition'])) {
+                    $output->writeln('condition OK !');
+                    continue;
+                }
             }
+            // on determine la question
+            $type                        = isset($item['type']) ? $item['type'] : 'string';
+            $question                    = $this->getQuestion($item['question'], $item['default'], $type);
+            $helper                      = $this->getHelper('question');
+            $answers[$item['attribute']] = $helper->ask($input, $output, $question);
+        }
+        $output->writeln(print_r($answers));
+    }
+
+    private function getQuestion($question, $default, $type)
+    {
+        if (strtolower($type) == 'string') {
+            return new Question($question, $default);
+        } elseif (strtolower($type) == 'bool') {
+            return new ChoiceQuestion(
+                $question,
+                array('oui', 'non'),
+                1
+            );
         }
     }
 
-    private function getDisclaimerAttributes()
+    private function checkCondition($reponse, $item)
+    {
+        $mode = strtolower($item['comparaison']);
+
+        if ($mode == 'equal') {
+            return ($item['value'] == $reponse);
+        }
+    }
+
+
+    private
+    function getInformations()
     {
         return array(
-            'siteUrl'           => 'The website URL ? (ex: www.exemple.com) ',
-            'ownerName'         => 'The website owner\'s name ? (ex: Bill) ',
-            'ownerStatus'       => 'The website owner\'s status ? (ex: director) ',
-            'ownerAddress'      => 'The website owner\'s address ? (ex: 23 baker-street avenue) ',
-            'ownerCompanyName'  => 'The website owner\'s company name ? (ex: Coca-cola) ',
-            'siteCreatorAgency' => 'The web agency name ? (ex: Web-flower) ',
-            'siteCreatorName'   => 'The project manager ? (ex: john Stanford) ',
-            'siteCreatorUrl'    => 'The web agency URL ? (ex: www.web-flower.com) ',
-            'webmasterName'     => 'The webmaster name ? (ex: Peter Folberg) ',
-            'editorManager'     => 'The editor manager ? (ex: Cindy Sanders) ',
-            'editorEmail'       => 'The editor email ? (ex: cindy.sander@email.com) ',
-            'hoster'            => 'The hoster ? (ex: TotalHosting) ',
-            'hosterAddress'     => 'The hoster address ? (ex: 45 blowfish road, San Diego) ',
-            'cnilNumber'        => 'The CNIL number ? (optionnal, for frenchs privacy rights)'
+            array(
+                'attribute' => 'siteUrl',
+                'question'  => 'Quelle est l\'URL du site web ?',
+                'default'   => 'www.disclaimer.net',
+            ),
+            array(
+                'attribute' => 'ownerName',
+                'question'  => 'Quel est le nom du proprietaire du site ?',
+                'default'   => 'Roger Freeman',
+            ),
+            array(
+                'attribute' => 'ownerStatus',
+                'question'  => 'Quel est le statut du proprietaire du site ?',
+                'default'   => 'Directeur',
+            ),
+            array(
+                'attribute' => 'ownerAddress',
+                'question'  => 'Quelle est l\'adresse du proprietaire du site ?',
+                'default'   => 'appt 43, Bat 4, Place Henry Fourrier, 23 250 Garantin',
+            ),
+            array(
+                'attribute' => 'ownerCompanyName',
+                'question'  => 'Quel est le nom de l`entreprise du site ?',
+                'default'   => 'La grande braderie',
+            ),
+            array(
+                'attribute' => 'siteCreatorAgency',
+                'question'  => 'Quelle est le nom de l\'agence web ?',
+                'default'   => 'WebCannibals',
+            ),
+            array(
+                'attribute' => 'siteCreatorName',
+                'question'  => 'Quelle est le nom du chef de projet ?',
+                'default'   => 'Raymond Gardonni',
+            ),
+            array(
+                'attribute' => 'siteCreatorUrl',
+                'question'  => 'Quelle est l\'URL du site de l\'agence ?',
+                'default'   => 'www.web-cannibal.net',
+            ),
+            array(
+                'attribute' => 'webmasterName',
+                'question'  => 'Quelle est le nom du webmaster ?',
+                'default'   => 'Billy McKullock',
+            ),
+            array(
+                'attribute' => 'editorManager',
+                'question'  => 'Quelle est le responsable d\'edition ?',
+                'default'   => 'Joshua Randall',
+            ),
+            array(
+                'attribute' => 'editorEmail',
+                'question'  => 'Quelle est l\'email du responsable d\'edition ?',
+                'default'   => 'Joshua.randall@paramount.com',
+            ),
+            array(
+                'attribute' => 'hoster',
+                'question'  => 'Quelle est l\'hebergeur ?',
+                'default'   => 'MaxHerberg2000',
+            ),
+            array(
+                'attribute' => 'hosterAddress',
+                'question'  => 'Quelle est l\'adresse de cet hebergeur ?',
+                'default'   => '14 rue du tigre 85330 Tourneville CEDEX 14',
+            ),
+            array(
+                'attribute' => 'cnil',
+                'question'  => 'Y a t il une declaration a la CNIL ?',
+                'default'   => false,
+                'type'      => 'bool'
+            ),
+            array(
+                'attribute' => 'cnilNumber',
+                'question'  => 'Le numero de declaration CNIL ?',
+                'default'   => '1234567890',
+                'condition' => array(
+                    'item'        => 'cnil',
+                    'comparaison' => 'equal',
+                    'value'       => true
+                )
+            )
         );
     }
 
-    private function getDefinitions()
+    protected
+    function interact(InputInterface $input, OutputInterface $output)
     {
-        $definitions = array();
-        foreach ($this->getDisclaimerAttributes() as $attributes => $textAnswer) {
-            $definitions[] = new InputArgument($attributes, InputArgument::REQUIRED, $attributes);
-        }
-        return $definitions;
     }
 
-    private function getDisclaimer()
+
+    private
+    function getDisclaimer()
     {
         $em   = $this->getEm();
         $repo = $em->getRepository('RudakDisclaimerBundle:DisclaimerData');
         return $repo->find(1);
     }
 
-    private function getEm()
+    private
+    function getEm()
     {
         if ($this->em == null) {
             $this->em = $this->getContainer()->get('doctrine')->getManager();
